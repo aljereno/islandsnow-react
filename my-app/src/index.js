a706fc533b87512c26b9db278e244ba64c959fc7
Aljaed Lui Ereno <alereno@hawaii.edu>
2019-10-14 18:35:44 -1000

    Task 2

---
+++ my-app/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+class IslandSnow extends React.Component {
+  render() {
+    return (
+      <h1>Island Snow!</h1>
+    );
+  }
+}
+
+ReactDOM.render(<IslandSnow />, document.getElementById('root'));
